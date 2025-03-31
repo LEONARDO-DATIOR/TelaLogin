ae5a8a7dd6338eb3c7d0e699ea464dfaddea0969
DATIOR.LEONARDO <leonardowederveiga@gmail.com>
2025-03-31 18:44:56 -0300

    Inicio do projetinho de login

---
+++ TelaLogin/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace TelaLogin
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ TelaLogin/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TelaLogin
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void MostrarSenha(object sender, RoutedEventArgs e)
+        {
+            if (CampoSenhaExibida.Visibility != Visibility.Visible) { 
+                CampoSenhaEscodida.Visibility = Visibility.Hidden;
+                CampoSenhaExibida.Text = CampoSenhaEscodida.Password;
+                CampoSenhaExibida.Visibility = Visibility.Visible;
+            }
+            
+        }
+
+        public void EscodeSenha(object sender, MouseEventArgs e)
+        {
+            CampoSenhaEscodida.Visibility = Visibility.Visible;
+            CampoSenhaExibida.Text = "";
+            CampoSenhaExibida.Visibility = Visibility.Hidden;
+        }
+    }
+}
